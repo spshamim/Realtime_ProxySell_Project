bb7165b1ef08b7a41656d6e251aeea1759408be1
Shahriar Parvez Shamim <spshamim90@gmail.com>
2024-11-13 21:24:21 +0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,12 +9,12 @@ export default function Contact() {
         <h2 className="text-[38px] font-bold text-[#ff1a1a] text-center mb-[20px] toolpage-head-shadow font-timmana">Contacts</h2>
         <ul className="list-none mt-[20px] font-secularone">
           <li className="my-5">
-            <a href="https://t.me/+IYQiMmNJL8kyZWY9" target="_blank" rel="noopener noreferrer" className="text-[#d82c2c] text-[18px] hover:text-[#ff4d4d] hover:underline">
+            <a href="" target="_blank" rel="noopener noreferrer" className="text-[#d82c2c] text-[18px] hover:text-[#ff4d4d] hover:underline">
               Join Our Telegram Main Group
             </a>
           </li>
           <li className="my-5">
-            <a href="https://t.me/+EtzFLJKNrQQ4NjVl" target="_blank" rel="noopener noreferrer" className="text-[#d82c2c] text-[18px] hover:text-[#ff4d4d] hover:underline">
+            <a href="" target="_blank" rel="noopener noreferrer" className="text-[#d82c2c] text-[18px] hover:text-[#ff4d4d] hover:underline">
               Join Our Telegram Support Group
             </a>
           </li>
